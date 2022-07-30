b79e7b74fdd7fa7f56b8b5bce6892bb613740e78
LoweSimon <simon.lowe90@hotmail.co.uk>
2022-07-30 21:01:59 +0100

    Adding code to repo

---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const projects = [
+    {
+        title: "DnD Currency Converter",
+        subtitle: "WIP",
+        description: "React app to convert DnD Currency",
+        // image: TO ADD IMAGE
+        link: "https://github.com/LoweSimon/DnDCurrencyConverter",
+    },
+    {
+        title: "Random Password Generator",
+        subtitle: "WIP",
+        description: "JavaScript app to generate a random password",
+        // image: TO ADD IMAGE
+        link: "https://github.com/LoweSimon/password-generator",
+    },
+    {
+        title: "Game API",
+        subtitle: "WIP",
+        description: "An API built on Node.js to provide information on video games",
+        // image: TO ADD IMAGE
+        link: "https://github.com/LoweSimon/GameAPI",
+    },
+];
+
+export const skills = [
+
+];
+
+export const testimonials = [
+
+];
