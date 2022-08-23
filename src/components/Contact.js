7c477d7c1813b68cba861cb0d8e0bb872eed4a4c
Simon <40033923+LoweSimon@users.noreply.github.com>
2022-08-23 10:42:07 +0100

    Updating file

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -72,8 +72,7 @@ export default function Contact() {
               Hire Me
             </h2>
             <p className="leading-relaxed mb-5">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-              suscipit officia aspernatur veritatis. Asperiores, aliquid?
+              Like what you see? Why not work with me to create the website just for you.
             </p>
             <div className="relative mb-4">
               <label htmlFor="name" className="leading-7 text-sm text-gray-400">
