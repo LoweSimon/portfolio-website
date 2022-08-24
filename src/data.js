9f6722c3465e51495d65f0c659aa233b87c34e91
LoweSimon <simon.lowe90@hotmail.co.uk>
2022-08-24 16:18:57 +0100

    Added further skills

--- src/data.js
+++ src/data.js
@@ -8,20 +8,6 @@ export const projects = [
         image: image,
         link: "https://github.com/LoweSimon/DnDCurrencyConverter",
     },
-    {
-        title: "Random Password Generator",
-        subtitle: "WIP",
-        description: "JavaScript app to generate a random password",
-        // image: TO ADD IMAGE
-        link: "https://github.com/LoweSimon/password-generator",
-    },
-    {
-        title: "Game API",
-        subtitle: "WIP",
-        description: "An API built on Node.js to provide information on video games",
-        // image: TO ADD IMAGE
-        link: "https://github.com/LoweSimon/GameAPI",
-    },
 ];
 
 export const skills = [
@@ -33,6 +19,9 @@ export const skills = [
     },
     {
         title: "CSS",
+    },
+    {
+        title: "Bootstrap",
     },
     {
         title: "React",
