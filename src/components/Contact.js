252e1b15ea71bac3dee02a3a5a65cc85476e48d9
LoweSimon <simon.lowe90@hotmail.co.uk>
2022-08-24 14:43:05 +0100

    Added onClick event

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -113,7 +113,8 @@ export default function Contact() {
             </div>
             <button
               type="submit"
-              className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+              onClick={handleSubmit}>
               Submit
             </button>
           </form>
