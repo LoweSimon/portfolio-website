58b2bce1d74cb124c60dce96c08979a09cb73478
LoweSimon <simon.lowe90@hotmail.co.uk>
2022-09-28 13:23:15 +0100

    Adjusting Navbar

--- src/components/Navmenu.js
+++ src/components/Navmenu.js
@@ -1,20 +1,31 @@
-import { ArrowRightIcon } from "@heroicons/react/solid";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import Container from 'react-boostrap/Container';
-import Navbar from 'react-boostrap/Navbar';
-import Nav from 'react-boostrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default function Navmenu()    {
     return  (
         <>
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark" expand="md" sticky="top">
                 <Container>
                     <Navbar.Brand href="#home">Simon Lowe</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.link href="#pastwork">Past Work</Nav.link>
-                        <Nav.link href="#skills">Skills</Nav.link>
-                        <Nav.link href="#workwithme">Work With Me</Nav.link>
+                    <Nav.Item>
+                        <Nav.Link href="#pastwork">Past Work</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#skills">Skills</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#workwithme">Work With Me</Nav.Link>
+                    </Nav.Item>
                     </Nav>
+                    </Navbar.Collapse>
+
+                    <FontAwesomeIcon icon="fa-brands fa-github"/>
                 </Container>
             </Navbar>
         </>
