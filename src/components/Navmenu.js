1d6df56aa2f98aab14d15f89bf0686e963efb4c2
LoweSimon <simon.lowe90@hotmail.co.uk>
2022-09-29 09:56:32 +0100

    commiting code

--- src/components/Navmenu.js
+++ src/components/Navmenu.js
@@ -12,20 +12,15 @@ export default function Navmenu()    {
                     <Navbar.Brand href="#home">Simon Lowe</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                    <Nav.Item>
-                        <Nav.Link href="#pastwork">Past Work</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="#skills">Skills</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
+                    <Nav className="me-auto">                   
+                        <Nav.Link href="#pastwork">Past Work</Nav.Link>                    
+                        <Nav.Link href="#skills">Skills</Nav.Link>                   
                         <Nav.Link href="#workwithme">Work With Me</Nav.Link>
-                    </Nav.Item>
                     </Nav>
                     </Navbar.Collapse>
-
+                    <a href="https://github.com/LoweSimon?tab=repositories">
                     <FontAwesomeIcon icon="fa-brands fa-github"/>
+                    </a>
                 </Container>
             </Navbar>
         </>
