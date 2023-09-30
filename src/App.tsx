70e3b28e32a2adf9e33e33661c3eb375a2c925b6
LoweSimon <simon.lowe90@hotmail.co.uk>
2023-09-30 17:10:55 +0100

    Set up project with Vite

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+import Navbar from './components/Navbar'
+
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <div>
+        Hello there
+      </div>
+    </>
+  )
+}
+
+export default App
