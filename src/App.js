da4f38c09c5f311fa95bc10b3d2b1c5cd59687ab
LoweSimon <simon.lowe90@hotmail.co.uk>
2024-02-23 12:56:19 +0000

    Adding banner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Banner from "./components/Banner";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navmenu from "./components/Navmenu";
@@ -10,6 +11,7 @@ export default function App() {
   return  (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navmenu />
+      <Banner />
       <About />
       <Projects />
       <Skills />
