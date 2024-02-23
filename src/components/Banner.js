51bf79d4710d1db5f41410778371b0ac7a4a6a6d
LoweSimon <simon.lowe90@hotmail.co.uk>
2024-02-23 12:56:36 +0000

    Adding banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -33,18 +33,13 @@ export default function Banner() {
           <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
             <circle cx={1} cy={1} r={1} />
           </svg>
-          Join us in Denver from June 7 – 9 to see what’s coming next.
+          Please note I am currently going through a re-brand. Keep your eyes peeled for a new and improvied website coming soon!
         </p>
-        <a
-          href="#"
-          className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
-        >
-          Register now <span aria-hidden="true">&rarr;</span>
-        </a>
+        
       </div>
       <div className="flex flex-1 justify-end">
         <button type="button" className="-m-3 p-3 focus-visible:outline-offset-[-4px]">
-          <span className="sr-only">Dismiss</span>
+          
           <XMarkIcon className="h-5 w-5 text-gray-900" aria-hidden="true" />
         </button>
       </div>
